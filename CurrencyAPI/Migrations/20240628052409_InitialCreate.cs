cf1a665bf3c92e1c97c51405388d382dd00aa601
ena <erkembekov.nurzhan@mail.ru>
2024-06-28 10:41:32 +0500

    Db migrations

---
+++ CurrencyAPI/Migrations/20240628052409_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CurrencyAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Code = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Value = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    ADate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
---
+++ CurrencyAPI/Migrations/20240628053146_ChangeTableName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyAPI.Migrations
+{
+    public partial class ChangeTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies");
+
+            migrationBuilder.RenameTable(
+                name: "Currencies",
+                newName: "R_CURRENCY");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_R_CURRENCY",
+                table: "R_CURRENCY",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_R_CURRENCY",
+                table: "R_CURRENCY");
+
+            migrationBuilder.RenameTable(
+                name: "R_CURRENCY",
+                newName: "Currencies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies",
+                column: "Id");
+        }
+    }
+}
