ecc7add7721d5c01aaf333d4e4361ba2e951a5b7
ena <erkembekov.nurzhan@mail.ru>
2024-06-28 16:23:24 +0500

    create currency repo

---
+++ CurrencyAPI/Repository/ICurrencyRepository.cs
@@ -0,0 +1,10 @@
+﻿using CurrencyAPI.Model;
+
+namespace CurrencyAPI.Repository
+{
+    public interface ICurrencyRepository
+    {
+        Task<int> SaveCurrencyData(DateTime date);
+        Task<List<Currency>> GetCurrencyData(DateTime date, string code = null);
+    }
+}
