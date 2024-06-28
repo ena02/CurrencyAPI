69e3414609a4a7819da1ec0cb86d307ddb359b01
ena <erkembekov.nurzhan@mail.ru>
2024-06-28 16:24:16 +0500

    create controller

---
+++ CurrencyAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,47 @@
+﻿using CurrencyAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyAPI.Controllers
+{
+
+    [Route("currency/")]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly CurrencyService _currencyService;
+
+        public CurrencyController(CurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+        }
+
+        [HttpGet("save/{date}")]
+        public async Task<IActionResult> SaveCurrencyData(DateTime date)
+        {
+            try
+            {
+                var count = await _currencyService.SaveCurrencyData(date);
+                return Ok(new { count });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Failed to save currency data: {ex.Message}" });
+            }
+        }
+
+        
+        [HttpGet("{date}/{code?}")]
+        public async Task<IActionResult> GetCurrencyData(DateTime date, string code = null)
+        {
+            try
+            {
+                var currencies = await _currencyService.GetCurrencyData(date, code);
+                return Ok(currencies);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Failed to retrieve currency data: {ex.Message}" });
+            }
+        }
+
+    } 
+}
--- CurrencyAPI/Program.cs
+++ CurrencyAPI/Program.cs
@@ -1,14 +1,24 @@
 using CurrencyAPI.DbContexts;
+using CurrencyAPI.Repository;
+using CurrencyAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<CurrencyService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Currency API", Version = "v1" });
+});
 builder.Services.AddDbContext<CurrencyDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -19,6 +29,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Currency API V1"));
+
 }
 
 app.UseHttpsRedirection();
