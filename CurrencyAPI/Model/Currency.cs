4f0cc72f70839dbb8357ffc75ba3d24822d57614
ena <erkembekov.nurzhan@mail.ru>
2024-06-28 10:40:37 +0500

    currency model

---
+++ CurrencyAPI/Model/Currency.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CurrencyAPI.Model
+{
+    public class Currency
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Code { get; set; }
+
+        [Required]
+        [Column(TypeName = "numeric(18,2)")]
+        public decimal Value { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime ADate { get; set; }
+    }
+}
